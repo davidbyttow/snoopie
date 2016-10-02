cf8f285160340e1eacc10a1df51e8d9334f3bdbf
David Byttow <david.byttow@gmail.com>
2016-10-02 16:57:04 -0700

    irst commit

--- background.js
+++ background.js
@@ -1,16 +1,50 @@
 
-function handleMessage(request, sender) {
-  if (request.found) {
-    chrome.browserAction.setIcon({path: 'red.png'});
-  } else {
-    chrome.browserAction.setIcon({path: 'gray.png'});    
-  }
-}
+var DEFAULT_TITLE = 'No snoops detected';
+var DEFAULT_ICON = 'snooptracker-inactive-16.png';
+var WATCHING_ICON = 'snooptracker-active-16.png';
 
-function check() {
-  chrome.tabs.executeScript(null, {file: "content_script.js"});
+function handleMessage(request, sender) {
+  var snoops = [];
+  if (request.foundFs) {
+    snoops.push('fullstory');
+  }
+  if (request.foundIn) {
+    snoops.push('inspectlet');
+  }
+  if (request.foundHj) {
+    snoops.push('hotjar');
+  }
+
+  var title = DEFAULT_TITLE;
+  var icon = DEFAULT_ICON;
+  var isWatching = snoops.length;
+  if (snoops.length) {
+    title = 'Snoops detected: ' + snoops.join(',');
+    icon = WATCHING_ICON;
+  }
+  chrome.browserAction.setIcon({ path: icon });
+  chrome.browserAction.setTitle({ title: title });
 }
 
 chrome.runtime.onMessage.addListener(handleMessage);
 
-chrome.browserAction.onClicked.addListener();
+function runTest() {
+  chrome.tabs.executeScript(null, {file: "content_script.js"});
+}
+
+chrome.browserAction.onClicked.addListener(runTest);
+chrome.tabs.onActivated.addListener(runTest);
+
+var timeout = null;
+
+chrome.tabs.onUpdated.addListener(function() {
+  runTest();
+
+  // Check again in a second to account for a slow load.
+  if (timeout === null) {
+    timeout = setTimeout(function() {
+      timeout = null;
+      runTest();
+    }, 1000);
+  }
+});
--- content_script.js
+++ content_script.js
@@ -1,2 +1,10 @@
-var found = document.documentElement.innerHTML.indexOf('_fs_namespace') >= 0;
-chrome.runtime.sendMessage({ found: found });
+var html = document.documentElement.innerHTML;
+var foundFs = html.indexOf('_fs_namespace') >= 0 || html.indexOf('www.fullstory.com/s/fs.js') >= 0;
+var foundIn = html.indexOf('cdn.inspectlet.com/inspectlet.js') >= 0;
+var foundHj = html.indexOf('static.hotjar.com/c/hotjar') >= 0;
+
+chrome.runtime.sendMessage({
+  foundFs: foundFs,
+  foundIn: foundIn,
+  foundHj: foundHj
+});
